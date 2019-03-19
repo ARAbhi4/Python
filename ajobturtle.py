bf25e8f06493e343963019b21d204c98cfd6a411
AnisurRahmanAbhi <anisurrahmanabhi@gmail.com>
2019-03-19 22:23:50 +0600

    add code

--- ajobturtle.py
+++ ajobturtle.py
@@ -2,13 +2,35 @@ import turtle
 
 class AjobTurtle(turtle.Turtle):
     """AjobTurtle is a class for new type of turtle"""
-    pass
+    def forward(self, distance):
+        super().backward(distance)
 
+
+    def back(self, distance):
+        super().forward(distance)
+
+    def left(self, angle):
+        super().right(angle)
+
+    def right(self, angle):
+        super().left(angle)
 
 if __name__ == "__main__":
     montu = AjobTurtle()
-    print(type(montu))
     montu.left(30)
     montu.forward(200)
+    montu.left(45)
+    montu.back(100)
+    montu.right(90)
+    montu.forward(10)
+
+    jhontu = turtle.Turtle()
+    jhontu.shape("turtle")
+    jhontu.left(30)
+    jhontu.forward(200)
+    jhontu.left(45)
+    jhontu.back(100)
+    jhontu.right(90)
+    jhontu.forward(10)
 
     turtle.done()
